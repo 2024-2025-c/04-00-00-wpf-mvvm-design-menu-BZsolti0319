9d48fba8c8218e53e5aaf7b3f0dbe568d0b55e27
BZsolti0319 <c9borzso@vasvari.org>
2024-11-14 17:46:09 +0100

    Add project files.

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using KretaBasicSchoolSystem.Desktop.Models;
+using System;
+using System.Net;
+
+namespace KretaBasicSchoolSystem.Desktop.Repositories
+{
+    public class UserRepository
+    {
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+            bool validUser = true;
+            return validUser;
+        }
+
+        public User? GetByUsername(string userName)
+        {
+             return new User
+             {
+                 Username = "user",
+                 Email = "user.mail@gmail.com",
+                 FirstName = "István",
+                 LastName = "User",
+                 Password = "pass111"
+             };
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,85 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FontAwesome.Sharp;
+using KretaBasicSchoolSystem.Desktop.ViewModels.Base;
+using KretaBasicSchoolSystem.Desktop.ViewModels.ControlPanel;
+using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolCitizens;
+using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolClasses;
+using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolSubjects;
+
+namespace KretaBasicSchoolSystem.Desktop.ViewModels
+{
+    public partial class MainViewModel : BaseViewModel
+    {
+        private ControlPanelViewModel _controlPanelViewModel;
+        private SchoolCitizensViewModel _schoolCitizensViewModel;
+        private SchoolClassesViewModel _SchoolClassesViewModel;
+        private SchoolSubjectsViewModel _SchoolSubjectsViewModel;
+
+        public MainViewModel()
+        {
+            _controlPanelViewModel = new ControlPanelViewModel();
+            _schoolCitizensViewModel = new SchoolCitizensViewModel();
+            _SchoolClassesViewModel = new SchoolClassesViewModel();
+            _SchoolSubjectsViewModel = new SchoolSubjectsViewModel();
+        }
+
+        public MainViewModel(
+            ControlPanelViewModel controlPanelViewModel,
+            SchoolCitizensViewModel schoolCitizensViewModel,
+            SchoolClassesViewModel schoolClassesViewModel,
+            SchoolSubjectsViewModel schoolSubjectsViewModel
+            )
+        {
+            _controlPanelViewModel = controlPanelViewModel;
+            _schoolCitizensViewModel = schoolCitizensViewModel;
+            _SchoolClassesViewModel = schoolClassesViewModel;
+            _SchoolSubjectsViewModel = schoolSubjectsViewModel;
+
+
+            CurrentChildView = _controlPanelViewModel;
+            ShowDashbord();
+        }
+
+        [ObservableProperty]
+        private string _caption = string.Empty;
+
+        [ObservableProperty]
+        private IconChar _icon = new IconChar();
+
+        [ObservableProperty]
+        private BaseViewModel _currentChildView;
+
+        [RelayCommand]
+        public void ShowDashbord()
+        {
+            Caption = "Vezérlőpult";
+            Icon=IconChar.SolarPanel;
+            CurrentChildView = _controlPanelViewModel;
+        }
+
+        [RelayCommand]
+        public void ShowSchoolCitizens()
+        {
+            Caption = "Iskolapolgárok";
+            Icon = IconChar.UserGroup;
+            CurrentChildView = _schoolCitizensViewModel;
+        }
+
+        [RelayCommand]
+        public void ShowSchoolClasses()
+        {
+            Caption = "Osztályok";
+            Icon = IconChar.FaceAngry;
+            CurrentChildView = _SchoolClassesViewModel;
+        }
+
+        [RelayCommand]
+        public void ShowSchoolSubjects()
+        {
+            Caption = "Tantárgyak";
+            Icon = IconChar.Bong;
+            CurrentChildView = _SchoolSubjectsViewModel;
+        }
+    }
+}
